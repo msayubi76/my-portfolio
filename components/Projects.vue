32d3d1ad9ffa4841323b5f1b51a2ebdc59fdd517
msayubi76 <47750619+msayubi76@users.noreply.github.com>
2025-01-31 23:23:31 +0500

    dfs

--- components/Projects.vue
+++ components/Projects.vue
@@ -5,77 +5,66 @@
         </h3>
         <h2 class="text-center">Projects I've Worked On </h2>
 
-        <div class="row">
-            <div class="col-md-4 mb-2" v-for="(project, index) in projects" :key="index">
-                <div class="py-3 px-2 pb-1 bg-black rounded-3">
-                    <NuxtLink :href="project.link" class="text-decoration-none">
-                        <p class="text-light text-opacity-75">
-                            {{ project.description }}
-                        </p>
 
-                        <img :src="project.image" class="img-fluid" />
-                    </NuxtLink>
-                    <h4 class="font-bold text-light text-center py-3">{{ project.name }}</h4>
-                </div>
 
-            </div>
-        </div>
-
-        <!-- <ClientOnly>
+        <ClientOnly>
             <swiper-container ref="containerRef">
                 <swiper-slide v-for="(project, index) in projects" :key="index">
                     <div class="py-3 px-2 pb-1 bg-black rounded-3">
-                        <NuxtLink :href="project.link" class="text-decoration-none">
+                        <!-- <NuxtLink :href="project.link" class="text-decoration-none"> -->
                             <p class="text-light text-opacity-75">
                                 {{ project.description }}
                             </p>
 
-                            <img :src="project.image" class="img-fluid" />
-                        </NuxtLink>
+                            <!-- <img :src="project.image" class="img-fluid" /> -->
+                        <!-- </NuxtLink> -->
                         <h4 class="font-bold text-light text-center py-3">{{ project.name }}</h4>
                     </div>
                 </swiper-slide>
             </swiper-container>
-        </ClientOnly> -->
+        </ClientOnly>
+
+
     </section>
 
 </template>
 
-<script setup lang="ts">
+<script setup>
+ 
 
-
-const projects = ref([
+const projects = [
     {
         name: 'Crypto Exchange Platform',
-        image: './images/project.png',
-        link: '/projects/crypto-exchange-platform',
+        // image: './images/project.png',
+        // link: '/projects/crypto-exchange-platform',
         description: "A full-featured crypto exchange platform developed  using Nuxt.js and Ant Design. Key features include P2P trading, spot/margin/futures trading, internal transfers, and decentralized deposits/withdrawals."
     },
     {
         name: 'PlutoPakistan',
-        image: './images/project.png',
-        link: '/projects/pluto-pakistan',
+        // image: './images/project.png',
+        // link: '/projects/pluto-pakistan',
         description: "Multivendor ecommerce platform  developed using Laravel, jQuery, and Nuxt 3. Key features include multi-payment options, return/refund processing, and a robust commission system for vendors and admin."
     },
     {
         name: 'E-Procurement',
-        image: './images/project.png',
-        link: '/projects/e-procurement',
+        // image: './images/project.png',
+        // link: '/projects/e-procurement',
         description: "Developed an E-Procurement Platform using Laravel and Vue.js. Key features include quotation management, vendor analysis, purchase order generation, and a built-in messaging system for seamless communication."
     },
     {
         name: 'Jdm Trading',
-        image: './images/project.png',
-        link: '/projects/jdm-trading',
+        // image: './images/project.png',
+        // link: '/projects/jdm-trading',
         description: "A car stock management platform built with Laravel, jQuery, and Bootstrap 5. Features include stock status labels, an admin panel for managing inventory, expenses, sales, and dynamic content."
     },
     {
         name: 'School Management',
-        image: './images/project.png',
-        link: '/projects/school-management',
+        // image: './images/project.png',
+        // link: '/projects/school-management',
         description: null
-    },
-])
+    }];
+
+
 
 
 // Create 10 slides
@@ -102,10 +91,5 @@ const swiper = useSwiper(containerRef, {
     },
 })
 
-onMounted(() => {
-    // Access Swiper instance
-    // Read more about Swiper instance: https://swiperjs.com/swiper-api#methods--properties
-    console.log(swiper.instance)
-})
 
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["~/assets/scss/custom-bootstrap.scss", "~/assets/custom.css"],
+  css: ["~/assets/scss/custom-bootstrap.scss", "~/assets/custom.css", "swiper/css"],
 
   build: {
     transpile: ["bootstrap"],
   },
-
+  // nitro: {
+  //   prerender: {
+  //     crawlLinks: false, // Stops Nuxt from auto-crawling links
+  //     routes: ["/projects"], // Explicitly include the /projects route
+  //   },
+  // },
   app: {
     baseURL: "/my-portfolio/", // Ensure this matches your repo name
   },
