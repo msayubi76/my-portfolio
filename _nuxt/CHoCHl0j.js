97c0e36e2a61b2ffb710b0c201112c2383b0410c
msayubi76 <47750619+msayubi76@users.noreply.github.com>
2025-02-02 18:10:37 +0500

    Updates

--- _nuxt/CHoCHl0j.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./CS1zLSIb.js";const n={};function t(r,s){return o(),c("div",null," school ")}const _=e(n,[["render",t]]);export{_ as default};
---
+++ _nuxt/GJfcC4Kr.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./C-bOFQrm.js";const n={};function t(r,s){return o(),c("div",null," school ")}const _=e(n,[["render",t]]);export{_ as default};
